756508cea40663f54e0d0dd226174369b9bb4f6b
Mitsuyasu Atsushi <ats.mtys@gmail.com>
2017-06-04 20:30:30 +0900

    ジャイロセンサー導入 　・まだちゃんと理解できていないので、うまく回転できない・・・ 　・ジャイロの値は取得して表示するようにしてあります

--- Tag/Assets/Network/PlayerMoveNetwork.cs
+++ Tag/Assets/Network/PlayerMoveNetwork.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
 
 public class PlayerMoveNetwork : NetworkBehaviour
 {
     public float speed = 0.01f;
-    public float turnSpeed = 1.0f;
+    public float turnSpeed = 5.0f;
 
     private PlayerManagerNetwork pmn;
     private static KeyCode turnRight = KeyCode.J;
@@ -15,11 +16,18 @@ public class PlayerMoveNetwork : NetworkBehaviour
 
     private CharacterController charaCtrl;
 
+    private Text gyroText;
+
+    private Quaternion preGyro;
+
     // Use this for initialization
     void Start()
     {
+        Input.gyro.enabled = true;
+        preGyro = Input.gyro.attitude;
         charaCtrl = gameObject.GetComponent<CharacterController>();
         pmn = GetComponent<PlayerManagerNetwork>();
+        gyroText = GameObject.Find("Canvas").transform.Find("GyroText").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -29,6 +37,8 @@ public class PlayerMoveNetwork : NetworkBehaviour
             InputKeyMove();
             InputKeyTurn();
             CrossPlatformInputMoveTranslate();
+            CrossPlatformTurn();
+            GyroTurn();
         }
     }
 
@@ -63,19 +73,49 @@ public class PlayerMoveNetwork : NetworkBehaviour
         if (Input.GetKey(turnRight) && !Input.GetKey(turnLeft))
         {
             //turn = 1f;
-            turn = -1; ;
+            turn = -1;
         }
         else if (Input.GetKey(turnLeft) && !Input.GetKey(turnRight))
         {
             //turn = -1;
-            turn = 1; ;
+            turn = 1;
         }
         else
         {
             turn = 0;
         }
 
-        transform.Rotate(0, turn * turnSpeed, 0);
+        //transform.Rotate(0, turn * turnSpeed, 0);
+        transform.rotation *= Quaternion.AngleAxis(turnSpeed, new Vector3(0, turn, 0));
     }
 
+    void CrossPlatformTurn() {
+        float x = CrossPlatformInputManager.GetAxisRaw("HorizontalCam");
+        //transform.Rotate(0, x * turnSpeed, 0);
+        transform.rotation *= Quaternion.AngleAxis(turnSpeed, new Vector3(0, x, 0));
+    }
+
+    void GyroTurn() {
+        /* iosはx軸回転が画面右倒ししたときの水平方向?
+           wの値もちゃんと考慮が必要そう　値の変化的に　難しい
+        */
+        float diff = 0;
+        Quaternion gyro = Input.gyro.attitude;
+
+        diff = gyro.x - preGyro.x;
+        //diff = gyro.y - preGyro.y;
+        //diff = gyro.z - preGyro.z;
+
+        gyroText.text = "Gyro"
+            + " x:" + gyro.x.ToString("f2") 
+            + " y:" + gyro.y.ToString("f2") 
+            + " z:" + gyro.z.ToString("f2") 
+            + " w:" + gyro.w.ToString("f2");
+
+        if (diff >= 0.005f || diff <= -0.005f) {
+            transform.rotation *= Quaternion.AngleAxis(turnSpeed, new Vector3(0, diff, 0));
+        }
+
+        preGyro = gyro;
+    }
 }
